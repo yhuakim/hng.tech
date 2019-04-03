ec6d6df361c670c709e3dd3e1d54d4bb5ee0dba5
Akin Aguda <lordtitan7@gmail.com>
2019-04-03 13:33:42 +0100

    changed to my page

--- views/akin.php
+++ views/akin.php
@@ -4,7 +4,7 @@
 		<?php include 'partials/navbar.php';?>
 		<div class="container">
 			<header class="pi-header">
-				<h1 class="h1">HNG 5.0 INTERNS</h1>
+				<h1 class="h1">My Page</h1>
 	  </header>
 	  <h1>I am an intern. My name is Akinwunmi</h1>
 </body>
