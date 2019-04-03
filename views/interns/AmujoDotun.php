47be5bdc05dd92268b098fb14d2c07f57d1e64ff
Amujo Dotun <samue;amujo@gmail.com>
2019-04-03 21:49:03 +0100

    Created My Profile

---
+++ views/interns/AmujoDotun.php
@@ -0,0 +1,40 @@
+
+
+<html>
+   <?php include 'partials/header.php';?>
+   <body class=" pi">
+      <?php include 'partials/navbar.php';?>
+      <section class="main-section">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-6 col-xs-12">
+                  <div class="col-md-6 col-xs-12">
+                     <div class="intern-block border-blue">
+                        <img src="https://res.cloudinary.com/dx9dvkfeg/image/upload/r_max/a_0/v1554312775/samples/Dotun/FB_IMG_1523307929915.jpg" alt="Amujo Oluwasomidotun">
+                        <h6>Amujo OLuwasomidotun</h6>
+                        <p>Back-end Engineer</p>
+                        <ul class="list">
+                           <li><a href="https://twitter.com/amujodotun" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="https://github.com/AmujoDotun" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="https://medium.com/@samuelamujo" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-xs-12 align-items-center">
+                     <div class="intern-bio border-blue">
+                        <h6>About me</h6>
+                        <p>Amujo Dotun is proactive individual with a passion for Information Technology career path,
+                         I aim to work in a progressive organization to use my skill so solve real life problem and also learn more to remain world class developer</p>
+                        <br />
+                        <h6>Skills</h6>
+                        <p>JavaScript | PHP | NodeJs</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+      </section>
+   </body>
+   <?php include 'partials/footer.php';?>
+</html>
