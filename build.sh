6c7f25c2b4488fac9c2dc10a7ec649fa47bbcb8f
timolinn <timothyonyiuke@gmail.com>
2019-04-03 23:02:53 +0100

    fix build script

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@
 count=$(git diff --cached --numstat | wc -l)
 
 if [ "$count" = 1 ]; then
-    echo "Pull request failed. Looks like you changed more than one file. Please go back to the instructions"
-    exit 1
+    echo "Success!"
+    exit 0
 fi
 
-echo "Success!"
-exit 0
+echo "Pull request failed. Looks like you changed more than one file. Please go back to the instructions"
+exit 1
