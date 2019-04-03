0b957a6142084ba494aa8cc0d95371cf07e17e4b
DavidSalami <wizdave97@gmail.com>
2019-04-03 17:57:19 +0100

    Added my profile David Salami

--- index.php
+++ index.php
@@ -44,6 +44,9 @@
       case '/interns/test' :
           require __DIR__ . '/views/interns/test.php';
           break;
+      case '/interns/david' :
+          require __DIR__ . '/views/david.php';
+          break;
       default:
           require __DIR__ . '/views/404.php';
           break;
