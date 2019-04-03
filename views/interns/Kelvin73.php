dbaba9686f0722b554e1039a779e70285c577d4a
Pheonix73 <36895007+Pheonix73@users.noreply.github.com>
2019-04-03 23:41:04 +0100

    Created New Profile

---
+++ views/interns/Kelvin73.php
@@ -0,0 +1,39 @@
+
+<html>
+   <?php include 'partials/header.php';?>
+   <body class=" pi">
+      <?php include 'partials/navbar.php';?>
+      <section class="main-section">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-6 col-xs-12">
+                  <div class="col-md-6 col-xs-12">
+                     <div class="intern-block border-blue">
+                        <img src="https://res.cloudinary.com/kelvin73/image/upload/v1554321080/aa84e079-5b95-4d18-a88e-b600b885c334.jpg" alt="Kelvin Pere">
+                        <h6>Kelvin Pere</h6>
+                        <p>Junior Front-End Developer and Android Developer</p>
+                        <ul class="list">
+                           <li><a href="https://twitter.com/Kswiss73Kswiss" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="https://github.com/Pheonix73" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="https://medium.com/@perekelvin66" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-xs-12 align-items-center">
+                     <div class="intern-bio border-blue">
+                        <h6>My name is Kelvin Pere, and i live in Nigeria. I have always been interested in technlogy and now of recent i have been enjoying writing code. 
+                        And it has been a wonderful learning experience to me and i will love to do more by suporting the community and giving back. 
+                        </h6>
+                        <br />
+                        <h6>Skills</h6>
+                        <p>JavaScript | Java| Android Development</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+      </section>
+   </body>
+   <?php include 'partials/footer.php';?>
+</html>
