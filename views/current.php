d25bf42625e30a8e98f74f9eac894a9d00e34b5c
Akin Aguda <lordtitan7@gmail.com>
2019-04-03 10:28:00 +0100

    Added Akinwunmi Aguda

--- views/current.php
+++ views/current.php
@@ -14,12 +14,12 @@
      			<div class="col-md-3 col-sm-6 col-xs-12">
      				<div class="intern-block border-blue">
      					<img src="../app/img/about-talent.png" alt="">
-     					  <h6>Mark Essien</h6>
-                 <p>Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+     					  <h6>Akinwunmi Aguda</h6>
+                 <p>I am an intern at HNG</p>
                  <ul class="list">
-                 <li><a href="#" target="_blank"<i class="fab fa-twitter"></i></a></li>
-                 <li><a href="#" target="_blank"<i class="fab fa-github"></i></a></li>
-                 <li><a href="#" target="_blank"<i class="fab fa-medium"></i></a></li>
+                 <li><a href="https://twitter.com/aguda_akin" target="_blank"<i class="fab fa-twitter"></i></a></li>
+                 <li><a href="https://github.com/AkinAguda" target="_blank"<i class="fab fa-github"></i></a></li>
+                 <li><a href="https://medium.com/@lordtitan7" target="_blank"<i class="fab fa-medium"></i></a></li>
                  </ul>
      					    <a href="#" class="btn-view">view profile</a>
      				</div>
