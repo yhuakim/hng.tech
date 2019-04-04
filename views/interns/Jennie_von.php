40fcb723d3b670b46afef633a50249d2765cb138
Jennifer <floxzyjay@gmail.com>
2019-04-04 02:44:50 +0100

    HNG Edit Commit

---
+++ views/interns/Jennie_von.php
@@ -0,0 +1,37 @@
+<html>
+   <?php include 'partials/header.php';?>
+   <body class=" pi">
+      <?php include 'partials/navbar.php';?>
+      <section class="main-section">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-6 col-xs-12">
+                  <div class="col-md-6 col-xs-12">
+                     <div class="intern-block border-blue">
+                        <img src="https://res.cloudinary.com/jennievon/image/upload/v1554336573/c83887fe-c1fb-4024-add4-2f4b65334d95.png" alt="Mark Essien">
+                        <h6>Jennifer Echenim</h6>
+                        <p>Front-end Developer</p>
+                        <ul class="list">
+                           <li><a href="https://twitter.com/Jennie_von" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="https://github.com/Jennievon" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="https://medium.com/@echenimjennifer" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-xs-12 align-items-center">
+                     <div class="intern-bio border-blue">
+                        <h6>About me</h6>
+                        <p>Hi! I'm Jennifer Echenim, a graduate of Electrical/Electronic Engineering. I am a front-end Web Developer aspiring to become a full-stack developer.</p>
+                        <br />
+                        <h6>Skills</h6>
+                        <p>HTML | CSS | JavaScript</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+      </section>
+   </body>
+   <?php include 'partials/footer.php';?>
+</html>
