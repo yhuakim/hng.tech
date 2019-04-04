e8133cd83a0f55006942d096c4297d30065d46bb
Hannah-28 <omomurewale@gmail.com>
2019-04-04 03:05:39 +0100

    initial commit

---
+++ views/interns/Anuoluwapo.php
@@ -0,0 +1,37 @@
+<html>
+   <?php include 'partials/header.php';?>
+   <body class=" pi">
+      <?php include 'partials/navbar.php';?>
+      <section class="main-section">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-6 col-xs-12">
+                  <div class="col-md-6 col-xs-12">
+                     <div class="intern-block border-blue">
+                        <img src="https://res.cloudinary.com/anuoluwapo/image/upload/v1554312952/img1.jpg" alt="Olurombi Anuoluwapo">
+                        <h6>Olurombi Anuoluwapo</h6>
+                        <p>Front-End Developer</p>
+                        <ul class="list">
+                           <li><a href="https://twitter.com/dev__girl" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="https://github.com/Hannah-28" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="https://medium.com/@oluronbianu" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-xs-12 align-items-center">
+                     <div class="intern-bio border-blue">
+                        <h6>About me</h6>
+                        <p>I am Anuoluwapo, a Front-End Developer based in Lagos, Nigeria. I produce high quality responsive websites and exceptional user experience. I enjoy listening to music and play games when I am not with my laptop.</p>
+                        <br />
+                        <h6>Skills</h6>
+                        <p>HTML5 | CSS3 | Javascript</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+      </section>
+   </body>
+   <?php include 'partials/footer.php';?>
+</html>
