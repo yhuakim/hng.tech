903cab621247de2e611b4d23fa38b435c8ab0fa2
Yhuakim <arinzeojili@gmail.com>
2019-04-04 02:11:46 -0700

    Yhuakim's Profile

---
+++ views/interns/yhuakim.php
@@ -0,0 +1,42 @@
+
+<html>
+   <?php include 'partials/header.php';?>
+   <body class=" pi">
+      <?php include 'partials/navbar.php';?>
+      <section class="main-section">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-6 col-xs-12">
+                  <div class="col-md-6 col-xs-12">
+                     <div class="intern-block border-blue">
+                        <img src="https://res.cloudinary.com/yhuakim/image/upload/v1554324134/myimages/IMG_20170923_173415_681.jpg" alt="yhuakim">
+                        <h6>Arinze Joachim</h6>
+                        <p>Front-end Engineer</p>
+                        <ul class="list">
+                           <li><a href="twitter.com/yhuakim" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="github.com/yhuakim" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="medium.com/@yhuakim" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-xs-12 align-items-center">
+                     <div class="intern-bio border-blue">
+                        <h6>About me</h6>
+                        <p>
+                            I am web developer, a Front-end engineer to be precise.
+                            I am very ethusiastic about web development and programming as a whole.
+                            This is why I joined the hotels.ng5.0 internship.
+                        </p>
+                        <br />
+                        <h6>Skills</h6>
+                        <p>JavaScript | HTML | CSS</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+      </section>
+   </body>
+   <?php include 'partials/footer.php';?>
+</html>
