870c9480a7dec362f0d13f0d3e3cc67719cefb98
Nura Shariff <shariffnura@gmail.com>
2019-04-04 01:44:54 +0100

    Added my page 'nura.php'

---
+++ views/interns/Nura.php
@@ -0,0 +1,37 @@
+<html>
+   <?php include 'partials/header.php';?>
+   <body class=" pi">
+      <?php include 'partials/navbar.php';?>
+      <section class="main-section">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-6 col-xs-12">
+                  <div class="col-md-6 col-xs-12">
+                     <div class="intern-block border-blue">
+                        <img src="https://res.cloudinary.com/daels4nmg/image/upload/v1554337270/nura.jpg" alt="Nura Musa Shariff">
+                        <h6>Nura Musa Shariff</h6>
+                        <p>Web and Android Developer</p>
+                        <ul class="list">
+                           <li><a href="https://twitter.com/shariffnour" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="https://github.com/shariffnour" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="https://medium.com/@nuramusashariff" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-xs-12 align-items-center">
+                     <div class="intern-bio border-blue">
+                        <h6>About me</h6>
+                        <p>I'm an introvert but i also enjoy other people's company at times. I like programming and would like to get better at it and become "worldclass", that's why i joined the internship.</p>
+                        <br />
+                        <h6>Skills</h6>
+                        <p>JavaScript | PHP | Java</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+      </section>
+   </body>
+   <?php include 'partials/footer.php';?>
+</html>
